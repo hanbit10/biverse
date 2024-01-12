b80ded3c0f74380c4372ef7025381fd778baa7b2
hanbit10 <chanbit10@gmail.com>
2024-01-12 14:08:23 +0100

    server changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:4200", "https://biverse.onrender.com"],
+    origin: ["https://biverse.onrender.com"],
   })
 );
 
