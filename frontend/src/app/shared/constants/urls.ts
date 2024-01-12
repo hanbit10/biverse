e9cd20f4dcbf3febea3b921dd27a4659d5e4b1ba
hanbit10 <chanbit10@gmail.com>
2024-01-12 14:25:49 +0100

    veres urls

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -3,11 +3,11 @@ import { environment } from 'src/environments/environment';
 // const BASE_URL = environment.production? '' : 'http://localhost:5000';
 const BASE_URL = environment.production ? '' : 'https://biverse.onrender.com';
 
-export const FOODS_URL = BASE_URL + '/api/foods';
-export const FOODS_TAGS_URL = FOODS_URL + '/tags';
-export const FOODS_BY_SEARCH_URL = FOODS_URL + '/search/';
-export const FOODS_BY_TAG_URL = FOODS_URL + '/tag/';
-export const FOOD_BY_ID_URL = FOODS_URL + '/';
+export const VERSES_URL = BASE_URL + '/api/foods';
+export const VERSES_TAGS_URL = VERSES_URL + '/tags';
+export const VERSES_BY_SEARCH_URL = VERSES_URL + '/search/';
+export const VERSES_BY_TAG_URL = VERSES_URL + '/tag/';
+export const VERSE_BY_ID_URL = VERSES_URL + '/';
 
 export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
 export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
